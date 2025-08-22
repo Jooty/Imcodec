d795766e822018e8acc9f4e77f4389faadf03153
Jooty <jay@ctrldevil.com>
2025-08-22 10:07:51 -0400

    fix: imcodec blob parse

--- src/Imcodec.Cli/Deserialization.cs
+++ src/Imcodec.Cli/Deserialization.cs
@@ -53,6 +53,17 @@ public static class Deserialization {
 
     public const string DeserializationSuffix = "_deser.json";
 
+    private static readonly List<uint> s_commonPropertyFlags = [
+        1, 6, 7, 16, 19, 23, 24, 25, 27, 30, 31, 39,
+        55, 59, 63, 71, 134, 135, 159, 263, 287, 519, 551,
+        575, 65536, 65543, 65567, 65664, 65671, 65695, 65703,
+        65727, 66087, 131079, 131103, 131207, 131335, 131359,
+        262151, 262279, 1048583, 2097159, 2097183, 2097215, 4194439,
+        8388615, 8388639, 8388871, 16777223, 16777247, 16777279,
+        33554439, 33554463, 33554471, 33554491, 33554495, 33554695,
+        33554719, 134217735, 134217759, 136314887, 268435463, 268435487,
+    ];
+
     /// <summary>
     /// Attempts to deserialize the given file data into a PropertyClass object. If successful, the
     /// deserialized object will be returned as a JSON string.
@@ -104,52 +115,67 @@ public static class Deserialization {
     /// <returns>The deserialized object as a JSON string, or null if deserialization failed.</returns>
     public static string? TryDeserializeHexBlob(string hexBlob) {
         // Remove spaces from the blob if they're present.
-        hexBlob = hexBlob.Replace(" ", string.Empty);
+        hexBlob = hexBlob
+            .Replace(" ", string.Empty)
+            .Replace("\n", string.Empty)
+            .Replace("\r", string.Empty);
         var buffer = Convert.FromHexString(hexBlob);
 
         // Create configurations for all serializers to try.
         var serializerConfigs = new List<(string Name, Func<ObjectSerializer> Factory, bool IsVerbose)> {
             // Standard serializers.
-            ("ObjectCompact",           static () => new ObjectSerializer(false, SerializerFlags.None), false),
-            ("ObjectCompactCompressed", static () => new ObjectSerializer(false, SerializerFlags.UseFlags | SerializerFlags.Compress), false),
-            ("ObjectVerbose",           static () => new ObjectSerializer(true, SerializerFlags.None), true),
-            ("ObjectVerboseCompressed", static () => new ObjectSerializer(true, SerializerFlags.UseFlags | SerializerFlags.Compress), true),
-
+            ("ObjectCompact",               static () => new ObjectSerializer(false, SerializerFlags.None), false),
+            ("ObjectCompactCompressed",     static () => new ObjectSerializer(false, SerializerFlags.Compress), false),
+            ("ObjectVerbose",               static () => new ObjectSerializer(true, SerializerFlags.None), true),
+            ("ObjectVerboseCompressed",     static () => new ObjectSerializer(true, SerializerFlags.Compress), true),
+            
             // Core serializers.
             ("CoreObject",                  static () => new CoreObjectSerializer(false, SerializerFlags.None), false),
-            ("CoreObjectCompressed",        static () => new CoreObjectSerializer(false, SerializerFlags.UseFlags | SerializerFlags.Compress), false),
+            ("CoreObjectCompressed",        static () => new CoreObjectSerializer(false, SerializerFlags.Compress), false),
             ("CoreObjectVerbose",           static () => new CoreObjectSerializer(true, SerializerFlags.None), true),
-            ("CoreObjectVerboseCompressed", static () => new CoreObjectSerializer(true, SerializerFlags.UseFlags | SerializerFlags.Compress), true)
+            ("CoreObjectVerboseCompressed", static () => new CoreObjectSerializer(true, SerializerFlags.Compress), true)
         };
 
         // Attempt to deserialize the blob using a number of methods.
         // Return the first successful deserialization.
-        try {
-            foreach (var (Name, Factory, IsVerbose) in serializerConfigs) {
-                var serializer = Factory();
-                PropertyClass? propertyClass;
+        foreach (var (Name, Factory, IsVerbose) in serializerConfigs) {
+            var serializer = Factory();
 
-                if (serializer.Deserialize(buffer, 1, out propertyClass!)) {
-                    var info = new DeserializedBlobInfo {
-                        _rawBlob = hexBlob,
-                        _deserializedOn = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
-                        _imcodecVersion = typeof(ArchiveCommands).Assembly.GetName()?.Version?.ToString() ?? "Unknown",      
-                        _flags = (uint) serializer.SerializerFlags,
-                        _serializerType = Name,
-                        _verbose = IsVerbose,
-                        _objectType = propertyClass.GetType().Name,
-                        _object = propertyClass
-                    };
+            foreach (var flag in s_commonPropertyFlags) {
+                try {
+                    if (serializer.Deserialize<PropertyClass>(buffer, flag, out var propertyClass)) {
+                        var info = new {
+                            _rawBlob = hexBlob,
+                            _deserializedOn = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+                            _imcodecVersion = typeof(ArchiveCommands).Assembly.GetName()?.Version?.ToString() ?? "Unknown",
+                            _flags = flag,
+                            _serializerType = Name,
+                            _verbose = IsVerbose,
+                            _objectType = propertyClass!.GetType().Name,
+                            _object = propertyClass
+                        };
 
-                    return JsonConvert.SerializeObject(info, Formatting.Indented);
+                        // Ensure that enums are written as strings.
+                        var jsonSerializerSettings = new JsonSerializerSettings {
+                            Converters = { new Newtonsoft.Json.Converters.StringEnumConverter() },
+                        };
+                        var jsonObj = JsonConvert.SerializeObject(info, Formatting.Indented, jsonSerializerSettings);
+
+                        return jsonObj;
+                    }
+                }
+                catch {
+                    continue;
                 }
             }
+        }
 
-            return null;
-        }
-        catch {
-            return null;
-        }
+        // If no deserialization was successful, return null.
+        Console.WriteLine("=== Deserialization failed ===");
+        Console.WriteLine($"Failed to deserialize hex blob: {hexBlob}");
+        Console.WriteLine("===============================");
+        
+        return null;
     }
 
 }
